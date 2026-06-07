68e4a514b97a10e01e9955b743e59ce9760692fa
Damien Magdangal <damienmagdangal.developer@gmail.com>
2026-06-07 15:53:23 +0800

    fix dependencies

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import ProjectComponent from "../components/ProjectComponent/Project";
+// import ProjectComponent from "../components/ProjectComponent/Project";
 
 const ProjectsPage = () => {
   return (
     <div className="flex mx-auto justify-center items-center">
-      <ProjectComponent />
+      {/* <ProjectComponent /> */}
     </div>
   );
 };
